334c2012562a61f15dfbfff8c44e2122bc3e7d68
Aahan Patel <aahanp@gmail.com>
2022-03-01 16:24:23 -0800

    Added function for connecting beziers

--- main.py
+++ main.py
@@ -13,6 +13,19 @@ def closestPoint(p0, bezier):
     return closestPoint
 
 
+def connectBezier(bezier1, bezier2):
+    p0 = bezier1[-1]
+    p1 = bezier2[0]
+    xdist = p0[0] - p1[0]
+    ydist = p0[1] - p1[1]
+    t1 = (p0[0] - (xdist / 3), p0[1] - (ydist / 3))
+    t2 = (p1[0] + (xdist / 3), p1[1] - (ydist / 3))
+    points = []
+    for k in range(0, numPoints):
+        points.append(cubicBezier(p0, t1, t2, p1, k / numPoints))
+    return points
+
+
 def lerp(p0, p1, t):
     x = (1 - t) * p0[0] + t * p1[0]
     y = (1 - t) * p0[1] + t * p1[1]
@@ -34,20 +47,11 @@ bezier1 = []
 bezier2 = []
 
 numPoints = 10
+
 for i in range(0, numPoints):
-    bezier1.append(cubicBezier((1, 0), (2, 1), (3, 1), (4, 0), i / numPoints))
+    bezier1.append(cubicBezier((5, 0), (6, 1), (7, 1), (8, 0), i / numPoints))
 for j in range(0, numPoints):
-    bezier2.append(cubicBezier((5, 0), (6, 1), (7, 1), (8, 0), j / numPoints))
+    bezier2.append(cubicBezier((10, 0), (11, 1), (12, 1), (13, 0), j / numPoints))
 
-p0 = bezier1[-1]
-p1 = bezier2[0]
-xdist = p0[0] - p1[0]
-ydist = p0[1] - p1[1]
-t1 = (p0[0] - (xdist / 3), p0[1] - (ydist / 3))
-t2 = (p1[0] + (xdist / 3), p1[1] - (ydist / 3))
-print(str(t1) + "," + str(t2))
-points = []
-for k in range(0, numPoints):
-    points.append(cubicBezier(p0, t1, t2, p1, k / numPoints))
-
+points = connectBezier(bezier1, bezier2)
 print(bezier1 + points + bezier2)
